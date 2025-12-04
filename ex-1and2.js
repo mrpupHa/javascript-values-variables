dd17f1c3ba28fd67a5df6094b883ff2cfae525c3
notapatda <notapat.dac@gmail.com>
2025-12-04 13:43:35 +0700

    feat: enhance ex-1and2.js with variable creation and reassignment, add temperature conversion in ex-3.js, and clarify expressions in ex-4.js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,26 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter"
+let lastName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const restaurantName = "Chef's Table by Chef James"
+const netBillAmount = 15000
+const activeRestaurant = 1500
+
+console.log(firstName)
+console.log(lastName)
+console.log(address)
+
+console.log(restaurantName)
+console.log(netBillAmount)
+console.log(activeRestaurant)
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+
+console.log(address)
+
+netBillAmount = 5000
+activeRestaurant = 2500
