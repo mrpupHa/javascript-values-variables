ae93e618ec53111cc053308eafde1cd96359e644
Nvp.Kcn <nvp.kcn@gmail.com>
2025-12-05 00:43:44 +0700

    feat: update ex-1and2.js with variable creation and reassignment, ex-3.js with direct calculation, and ex-4.js with expression comments

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,21 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 BangKruay-Sainoi Bang Kruay BangKruai";
+
+console.log(firstName); // Karter
+console.log(surName); // Anderson
+console.log(address); // 54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai
+
+const nameRestaurant = "Chef's Table by Chef James";
+const netBillPrice = 15000;
+const totalActiveRestaurant = 1500;
+
+console.log(nameRestaurant); // Chef's Table by Chef James
+console.log(netBillPrice); // 15000
+console.log(totalActiveRestaurant); // 1500
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 CountyRd #4Meeker, Colorado(CO),81641"
+netBillPrice = 5000;
+totalActiveRestaurant = 2500;
--- ex-4.js
+++ ex-4.js
@@ -2,12 +2,14 @@
 50 + 12; // Expression
 
 //Exercise #4: What is Expression Start coding here
-130 + 300;
-let zero = 1000 - 1000;
-20 * 120;
-1000 / 2;
-typeof true;
+
+130 + 300; // Expreessions
+let zero = 1000 - 1000; //
+20 * 120; // Expreessions
+1000 / 2; // Expreessions
+typeof true; // Expreessions
 let name = "John";
-3 > 5;
-10 == 100;
-200;
+3 > 5; // Expreessions
+10 == 100; // Expreessions
+200; // Expreessions
+
